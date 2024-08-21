03f79967c1f7a536b7db1d414c1c9d6a9867178f
LastCow9000 <sys19912002@hanmail.net>
2024-08-21 16:46:35 +0900

    feat: db 설계에 따른 entitiy 추가

---
+++ src/main/java/com/rgt/assignment/constants/OrderStatus.java
@@ -0,0 +1,7 @@
+package com.rgt.assignment.constants;
+
+public enum OrderStatus {
+    CONFIRMED,
+    CANCELED,
+    COMPLETED
+}
--- src/main/java/com/rgt/assignment/entity/User.java
+++ src/main/java/com/rgt/assignment/entity/User.java
@@ -6,16 +6,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
@@ -24,21 +18,11 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @NoArgsConstructor
 @AllArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
-public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
+public class User extends BaseEntity {
     @Column(unique = true)
     private String username;
     private String password;
 
     @Enumerated(EnumType.STRING)
     private Role role;
-
-    @CreatedDate
-    private LocalDateTime createdAt;
-
-    @LastModifiedDate
-    private LocalDateTime updatedAt;
 }
